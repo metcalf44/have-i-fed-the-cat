9f0bba8bf5fd13f3ed481e5395a37497182d9d62
Ernie <emetcalf44@gmail.com>
2021-05-21 09:28:06 +0100

    updtes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(`app is listening on localhost:${PORT}`)
+app.listen(3000, () => {
+    console.log(`app is listening on localhost:3000`)
 });
